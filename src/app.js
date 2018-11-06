289ab2dcacb84e6fc3651458985b0b4067333fcc
Daniil <danixoon@gmail.com>
2018-11-06 22:05:32 +0300

    key

--- src/app.js
+++ src/app.js
@@ -4,7 +4,7 @@ var port = process.env.PORT || 8080;
 
 
 app.set("view engine", "ejs");
-app.use("/public", express.static(__dirname + "/public"));
+app.use("/public", express.static("./public"));
 app.get(
   "/user/:id",
   function(req, res, next) {
